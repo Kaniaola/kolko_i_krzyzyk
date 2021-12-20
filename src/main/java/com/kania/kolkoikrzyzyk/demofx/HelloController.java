d744708f9036fbe61e99f285f543fa5dadc2c804
Aleksandra Kania <ola.kania1@gmail.com>
2021-12-20 22:56:02 +0100

    Initial commit

---
+++ src/main/java/com/kania/kolkoikrzyzyk/demofx/HelloController.java
@@ -0,0 +1,14 @@
+package com.kania.kolkoikrzyzyk.demofx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
